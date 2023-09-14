f002ae2fbe322188f02cdb7e0a7253e37ee9f442
r1tter <marcelosori@hotmail.com>
2023-09-14 10:14:06 -0300

    Setup: initial tailwind configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
   )
 }
